8c621fee378cd7089ec2d91629cf48c471a9865e
Bas aan de Wiel <bas.aan.de.wiel@gmail.com>
2022-04-03 19:56:14 +0200

    updated comments

--- src/main.c
+++ src/main.c
@@ -1,17 +1,14 @@
 /*
- * Copyright (c) 2020 Intel Corporation.
+ * Peripheral sends advertising packets during certain amount of time every 100msec (defautl inverval of Zephyr).
+ * When there is still no connection after this period of sending advertisement, the device goes into sleep mode 
+ * to conserve power for a certain period of time.
+ * When the sleep period is over, it starts again sending advertisements during certain amount of time.
+ * etc. etc.
  *
- * SPDX-License-Identifier: Apache-2.0
- * Psuedo Code: 
- * &1. Startup 
- * &2. Enable ESP32 as a ble peripheral 
- * &3. Esp32 send an advertising packet 1x per minute containing device serial #, 
- *    waits 500ms for a response. 
- *    baswi: only one adv packet gives small chance for connection; I send adv packets with default inverval of Zephyr (100msec)
- * 			 during 500 ms (2 sec for testing purposes)   
- * &If no response is found esp32 goes to light sleep with ble modem off to conserve power (battery-based application) 
- * &4. If a connection is available the esp32 will connect and send a 2kb json packet (broken down into chunks 
- * 5. Receiving python code end needs to re-assemble data do form original packet Deliverables Working Python and ESP-IDF Code (no android)
+ * If a connection is available the device will send big data packet via BLE. This data packet is splitted into chunks
+ * because it is bigger than the MTU. Extra information is added so that the receiving side is able to reassemble the chunks.
+ *
+ * The central part that is reassembling the chunks is also available as a Python script.
  */
 
 #include <zephyr.h>
@@ -29,7 +26,7 @@ bool periphConnected = false;
 int err; //used in different functions
 uint16_t CurrentMTUrx, CurrentMTUtx; //used to save current MTU
 
-K_SEM_DEFINE(sem_wakeup, 0, 1);
+K_SEM_DEFINE(sem_wakeup, 0, 1); //semaphore to time sending of BLE advertisements
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -47,7 +44,6 @@ void mtu_updated(struct bt_conn *conn, uint16_t tx, uint16_t rx)
 	CurrentMTUtx = tx;
 }
 
-//baswi added
 static struct bt_gatt_cb gatt_callbacks = {
 	.att_mtu_updated = mtu_updated
 };
@@ -58,7 +54,7 @@ static void bt_connected(struct bt_conn *conn, uint8_t err)
 	if (err) {
 		printk("Connection failed (err 0x%02x)\n", err);
 	} else {
-		printk("*** Something connected to this peripheral; pairing not necessary\n");
+		printk("*** Something connected to this peripheral\n");
 		periphConnected = true;
 	}
 }
@@ -83,24 +79,7 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	//.security_changed = security_changed,
 	//.identity_resolved = identity_resolved,
 	
-	//@@@bt_gatt_notify_cb  is called when the packet has been ACKed on the link layer
-
-	//@@@ I can send data to the BLE Client by calling the function bt_gatt_notify_cb (sends notify packets)
-	//modified the MTU in order to be able to send more than the 23 Bytes
-	// loop that calls the bt_gatt_notify_cb function as many times as data sets are to be sent.
-	//answer: Yes, you can do that. It could make sense to wait for the callback before you send more in this case.
-	//
-	//whether notifications or indications should be used. 
-	//The only reason to use indications is if you need confirmation in the application layer that the indication has been received. 
-	//This is only useful in some very specific cases.
-	//Notifications "are still guaranteed to reach the target because of the link layer ACK
-	//Yes, the link layer (which is either the Zephyr link layer or the SoftDevice controller) keeps track of transmitted packets and if they are acknowledged or not.
-	//If retransmissions keep failing so that there are no packets exchanged during a supervision timeout (which is a basic connection parameter), the link will be closed due to supervision timeout. 
-	//In that case the application will be notified that a supervision timeout has occurred.
-	//No. You cannot send notifications (or indications) without a connection, and even if connected, the client needs to have enabled notifications/indications. 
-	//It is correct and appropriate that the bt_gatt_notify_cb() function returns an error in this case.
-	// bt_gatt_notify_cb  is called when the packet has been ACKed on the link layer. Any retransmissions etc happen in the link layer without the host knowing about it.
-
+	//bt_gatt_notify_cb  is called when the packet has been ACKed on the link layer - not used
 };
 
 
@@ -118,7 +97,6 @@ static struct bt_conn_auth_cb auth_cb_display = {
 };
 
 
-//baswi added
 #define GATT_PERM_READ_MASK     (BT_GATT_PERM_READ | \
 				 BT_GATT_PERM_READ_ENCRYPT | \
 				 BT_GATT_PERM_READ_AUTHEN)
@@ -149,7 +127,7 @@ static void ccc_changed(void)
 	k_sleep(K_SECONDS(5));
 }
 
-/* Heart Rate Service Declaration */
+/* Service Declaration */
 BT_GATT_SERVICE_DEFINE(hrs_svc,
 	BT_GATT_PRIMARY_SERVICE(BT_UUID_HRS),
 	BT_GATT_CHARACTERISTIC(BT_UUID_HRS_MEASUREMENT, BT_GATT_CHRC_NOTIFY,
@@ -158,9 +136,6 @@ BT_GATT_SERVICE_DEFINE(hrs_svc,
 											  //without this macro no notifications are sent by peripheral
 											  //1st param: Configuration changed callback
 	//https://nl.mathworks.com/help/matlab/import_export/work-with-device-characteristics-and-descriptors.html
-	//After connecting to your device, you can interface with it by reading or writing the device characteristics and 
-	//descriptors.
-	//Attributes can vary from read, write, notify, indicate
 );
 
 
@@ -170,56 +145,41 @@ static void data_notify(void)
 
 	char dataToBeSent[] = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
 	char*  datapart = k_malloc(CurrentMTUtx); //allocate memory for datapart
+	
 	printk("CurrentMTUtx= %d\n", CurrentMTUtx);
 
-	//insert <TAG><LENGTH>, so that receiving part can reassemble the data chunks
-	//result is <TAG><LENGTH><data>; this will be split in chunks if necessary
-	//@@@increase length by: <TAG>=1byte; <LENGTH>=4bytes=total length of data to be sent
-	unsigned char TAG=0; //not yet used
+	unsigned char ChunkNbr=0; //Sequence number, starts with zero
 	unsigned char MoreDataFollows=0; //indicates whether more data chunks follow;
-									 //equals zero for last data chunk, or data consists of only one chunk
-	uint16_t LENGTH = sizeof(dataToBeSent); //length of payload to be sent in bytes
-	int16_t lengthToBeSent = sizeof(TAG) + sizeof(MoreDataFollows) + sizeof(LENGTH) + LENGTH; //number of bytes *still* to be sent; can become negative in loop
-							 //is NOT correct; EVERY chunk sent contains extra byte MoreDataFollows@@@
-												   //for a string this is INCLUDING NULL character
+									 //equals to zero for last data chunk, or if data consists of only one chunk
+	unsigned char ProtocolOverhead = sizeof(ChunkNbr) + sizeof(MoreDataFollows); //overhead in each chunk
+	
+	uint16_t nbrBytesPayloadToBeSent = sizeof(dataToBeSent);	//nbr of bytes payload *still* to be sent
 	uint16_t nbrBytesPayloadAlreadySent = 0;				//nbr of bytes payload that are already sent
-	while (lengthToBeSent >0) {
-		printk("lengthToBeSent= %d\n", lengthToBeSent); 
-		uint16_t chunkSize = (CurrentMTUtx-4); //@@@4 bytes overhead for L2CAP header
-		if (lengthToBeSent <= (CurrentMTUtx-4)) { //if <= max chunksize bytes need to be sent
-			chunkSize = lengthToBeSent;
-			MoreDataFollows = 0; //false; necesary for last chunk
+
+	while (nbrBytesPayloadToBeSent >0) {
+		printk("nbrBytesPayloadToBeSent= %d\n", nbrBytesPayloadToBeSent); 
+		uint16_t chunkSize = (CurrentMTUtx-4); //4 bytes overhead for L2CAP header
+		if ((nbrBytesPayloadToBeSent + ProtocolOverhead) <= chunkSize) { //if <= max chunksize bytes need to be sent
+			chunkSize = nbrBytesPayloadToBeSent + ProtocolOverhead;
+			MoreDataFollows = 0; //false
 		}
 		else {
 			MoreDataFollows = 1; //true
 		}
 		printk("chunkSize= %d\n", chunkSize); 
+		printk("ChunkNbr= %d\n", ChunkNbr); 
+
+        //Data PDU: <ChunkNbr> <MoreDataFollows> <payload>		
+		memcpy(datapart, &ChunkNbr, sizeof(ChunkNbr)); //insert chunkNbr
+		ChunkNbr++;
+		memcpy(datapart+sizeof(ChunkNbr), &MoreDataFollows, sizeof(MoreDataFollows)); //insert MoreDataFollows flag
+		memcpy(datapart+sizeof(ChunkNbr)+sizeof(MoreDataFollows), (dataToBeSent+nbrBytesPayloadAlreadySent), (chunkSize - ProtocolOverhead));   	//copy chunksize data to local data 
+		nbrBytesPayloadAlreadySent += (chunkSize - ProtocolOverhead); //substract protocol overhead from chunkSize
+		nbrBytesPayloadToBeSent -= (chunkSize - ProtocolOverhead);
 		
-		if (nbrBytesPayloadAlreadySent == 0) { //if first chunck-> insert TAG and LENGTH and MoreDataFollows
-			memcpy(datapart, &TAG, sizeof(TAG));
-			memcpy(datapart+sizeof(TAG), &LENGTH, sizeof(LENGTH));
-			memcpy(datapart+sizeof(TAG)+sizeof(LENGTH), &MoreDataFollows, sizeof(MoreDataFollows));
-			memcpy(datapart+sizeof(TAG)+sizeof(LENGTH)+sizeof(MoreDataFollows), (dataToBeSent+nbrBytesPayloadAlreadySent), chunkSize-sizeof(TAG)-sizeof(LENGTH)-sizeof(MoreDataFollows)); 	//copy chunksize data to local data 
-			if (MoreDataFollows) {
-				lengthToBeSent -= (chunkSize - sizeof(MoreDataFollows)); //MoreDataFollows byte must be sent also in next chunk
-			} else {
-				lengthToBeSent -= chunkSize;
-			}
-			nbrBytesPayloadAlreadySent = chunkSize - sizeof(TAG) - sizeof(LENGTH) - sizeof(MoreDataFollows);
-		} else {
-			//only insert MoreDataFollows byte in front of payload
-			memcpy(datapart, &MoreDataFollows, sizeof(MoreDataFollows));
-			memcpy(datapart+sizeof(MoreDataFollows), (dataToBeSent+nbrBytesPayloadAlreadySent), chunkSize); 	//copy chunksize data to local data 
-			if (MoreDataFollows) {
-				lengthToBeSent -= (chunkSize - sizeof(MoreDataFollows)); //MoreDataFollows byte must be sent also in next chunk
-			} else {
-				lengthToBeSent -= chunkSize;
-			}
-			nbrBytesPayloadAlreadySent += (chunkSize - sizeof(MoreDataFollows));
-		}
-		rc = bt_gatt_notify(NULL, &hrs_svc.attrs[1], datapart, chunkSize); //%%% can I delete the other attributes?
-    	    //1st param=Connection object;if NULL notify all peer that have notification enabled via CCC @@@
-			//							 otherwise do a direct notification only the given connection.
+		rc = bt_gatt_notify(NULL, &hrs_svc.attrs[1], datapart, chunkSize); //can I delete the other attributes?
+    	    //1st param=Connection object;if NULL notify all peer that have notification enabled via CCC
+			//    						 otherwise do a direct notification only the given connection.
         	//&hrs_svc_attrs[1] – Characteristic or Characteristic Value attribute. [1]=first? attribute??
         	//datapart – Pointer to Attribute data.
        	 	//chunkSize – Attribute value length.
@@ -308,9 +268,8 @@ static void thread1_entry(void *p1, void *p2, void *p3)
 {
 	while (1) {
 		k_sem_give(&sem_wakeup); //start advertising
-		printk("TRHEAD1: sleep for 30 seconds\n");
+		printk("TRHEAD1: sleep for x seconds\n");
 		k_sleep(K_SECONDS(5)); 
-		//k_sleep(K_MSEC(500));
 	}
 }
 K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1_entry, NULL, NULL, NULL,
@@ -322,7 +281,7 @@ void main(void)
 	priv_bt_enable();
 	printk("*** Register authentication call backs\n");
 
-	bt_gatt_cb_register(&gatt_callbacks); 	//needed for fi mtu_updated call back function
+	bt_gatt_cb_register(&gatt_callbacks); 	//needed for mtu_updated call back function
 	bt_conn_auth_cb_register(&auth_cb_display);
 
 	//struct bt_gatt_attr *vnd_ind_attr; //baswi added
@@ -332,7 +291,7 @@ void main(void)
 	while (1) {
 		while (!periphConnected) { //while this peripheral is not connected
 			//wait till sem available->send advertisement during x seconds
-			if  (k_sem_take(&sem_wakeup, K_FOREVER) == 0) { //probably sleep iso sem was also OK to guarantee sleeping
+			if  (k_sem_take(&sem_wakeup, K_FOREVER) == 0) { //sleep iso sem was also OK to guarantee sleeping
 				priv_bt_le_adv_start();
 				k_sleep(K_SECONDS(2)); //advertise during x seconds
 				priv_bt_le_adv_stop();
