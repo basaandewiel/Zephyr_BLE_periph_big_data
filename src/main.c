503eda8b1bac3db3ec73e18e0b0cf66d79bb74ce
Bas aan de Wiel <bas.aan.de.wiel@gmail.com>
2022-10-29 20:49:17 +0200

    inserted zephyr/ before include paths

--- src/main.c
+++ src/main.c
@@ -14,10 +14,10 @@
 //#include <sys/printk.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/conn.h>
-#include <bluetooth/uuid.h>
-#include <bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
 //#include <bluetooth/services/bas.h>
 //#include <bluetooth/services/hrs.h>
 
